7185afa8f5eaa171bd62672613209279ffe1b792
dlatikay <53712466+dlatikaynen@users.noreply.github.com>
2021-03-12 10:11:53 +0100

    Update AppMinimize.js

--- www/AppMinimize.js
+++ www/AppMinimize.js
@@ -1,10 +1,14 @@
-var appMinimize = {
+var appMinimize2 = {
   minimize: function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'AppMinimize', 'minimize', []);
+  }
+  
+  sentToBackground: function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'AppMinimize', 'sendToBackground', []);  
   }
 }
 
 if (!window.plugins) { window.plugins = {}; }
 
-window.plugins.appMinimize = appMinimize;
-return window.plugins.appMinimize;
+window.plugins.appMinimize2 = appMinimize2;
+return window.plugins.appMinimize2;
