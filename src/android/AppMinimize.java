8a008464cf869e6d76c2af5454272153600472b1
dlatikay <53712466+dlatikaynen@users.noreply.github.com>
2021-03-12 11:04:02 +0100

    Update AppMinimize.java

--- src/android/AppMinimize.java
+++ src/android/AppMinimize.java
@@ -6,7 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.content.Intent;
 
-public class AppMinimize2 extends CordovaPlugin
+public class AppMinimize extends CordovaPlugin
 {
 	@Override
 	public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException
